a0347ef95efe098365d3b36499bea49c944d485e
ks734 <karthick_s@comcast.com>
2025-02-14 09:08:45 +0000

    RDKEMW-1639: Bring L1/L2 tests

---
+++ .github/workflows/BuildThunderWithRdkservicesApis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+
+############################
+# EXPERIMENTAL
+# DO NOT MODIFY
+
+############################
+# 1. Install Dependencies
+
+sudo apt install -y build-essential cmake ninja-build libusb-1.0-0-dev zlib1g-dev libssl-dev
+
+pip install jsonref
+
+############################
+# 2. Build Thunder Tools
+
+git clone https://github.com/rdkcentral/ThunderTools.git
+
+cmake -G Ninja -S ThunderTools -B build/ThunderTools -DCMAKE_INSTALL_PREFIX="install"
+
+cmake --build build/ThunderTools --target install
+
+############################
+# 3. Build Thunder
+
+git clone https://github.com/rdkcentral/Thunder.git
+
+cmake -G Ninja -S Thunder -B build/Thunder \
+  -DBINDING="127.0.0.1" \
+  -DCMAKE_BUILD_TYPE="Debug" \
+  -DCMAKE_INSTALL_PREFIX="install" \
+  -DPORT="55555" \
+  -DTOOLS_SYSROOT="${PWD}" \
+  -DINITV_SCRIPT=OFF
+
+cmake --build build/Thunder --target install
+
+############################
+# 4. Build rdkservices-apis
+
+git clone https://github-actions:${GITHUB_TOKEN}@github.com/rdk-e/rdkservices-apis.git
+
+cmake -G Ninja -S rdkservices-apis -B build/rdkservices-apis \
+  -DCMAKE_INSTALL_PREFIX="install"
+
+cmake --build build/rdkservices-apis --target install
