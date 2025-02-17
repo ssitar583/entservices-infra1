9c3168edf1b397c92f8e297beaaeaa97fe63def5
Mateusz Hobgarski <Mateusz_Hobgarski@comcast.com>
2025-02-17 15:42:17 +0000

    RDKEMW-1594: Add Thunder Interface for OCIContainer
    Reason for change: Add Thunder Interface for OCIContainer
    
    Test Procedure: NA
    Risk: Low
    Priority: P2
    Signed-off-by: Mateusz Hobgarski <mateusz.hobgarski@consult.red>

---
+++ OCIContainer/DobbyInterface.h
@@ -0,0 +1,76 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2024 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#pragma once
+
+#include <Dobby/DobbyProtocol.h>
+#include <Dobby/Public/Dobby/IDobbyProxy.h>
+#include <vector>
+#include <map>
+#include <i_omi_proxy.hpp>
+#include "IEventHandler.h"
+
+namespace WPEFramework
+{
+namespace Plugin
+{
+    class DobbyInterface
+    {
+        public:
+            DobbyInterface();
+            virtual ~DobbyInterface();
+            DobbyInterface(const DobbyInterface &) = delete;
+            DobbyInterface &operator=(const DobbyInterface &) = delete;
+        
+            bool initialize(IEventHandler* eventHandler);
+            void terminate();
+
+            bool listContainers(string& containers, string& errorReason);
+            bool getContainerInfo(const string& containerId, string& info, string& errorReason);
+            bool getContainerState(const string& containerId, string& state, string& errorReason);
+            bool startContainer(const string& containerId, const string& bundlePath, const string& command, const string& westerosSocket, uint32_t& descriptor, string& errorReason);
+            bool startContainerFromDobbySpec(const string& containerId, const string& dobbySpec, const string& command, const string& westerosSocket, uint32_t& descriptor, string& errorReason);
+            bool stopContainer(const string& containerId, bool force, string& errorReason);
+            bool pauseContainer(const string& containerId, string& errorReason);
+            bool resumeContainer(const string& containerId, string& errorReason);
+            bool hibernateContainer(const string& containerId, const string& options, string& errorReason);
+            bool wakeupContainer(const string& containerId, string& errorReason);
+            bool executeCommand(const string& containerId, const string& options, const string& command, string& errorReason);
+            bool annotate(const string& containerId, const string& key, const string& value, string& errorReason);
+            bool removeAnnotation(const string& containerId, const string& key, string& errorReason);
+            bool mount(const string& containerId, const string& source, const string& target, const string& type, const string& options, string& errorReason);
+            bool unmount(const string& containerId, const string& target, string& errorReason);
+        
+            void onContainerStarted(int32_t descriptor, const std::string& name);
+            void onContainerStopped(int32_t descriptor, const std::string& name);
+            void onVerityFailed(const std::string& name);
+        
+        private:
+            int mEventListenerId; // Dobby event listener ID
+            long unsigned mOmiListenerId;
+            std::shared_ptr<IDobbyProxy> mDobbyProxy; // DobbyProxy instance
+            std::shared_ptr<AI_IPC::IIpcService> mIpcService; // Ipc Service instance
+            const int GetContainerDescriptorFromId(const std::string& containerId);
+            static const void stateListener(int32_t descriptor, const std::string& name, IDobbyProxyEvents::ContainerState state, const void* _this);
+            static const void omiErrorListener(const std::string& id, omi::IOmiProxy::ErrorType err, const void* _this);
+            std::shared_ptr<omi::IOmiProxy> mOmiProxy;
+            IEventHandler* mEventHandler;
+    };
+} // namespace Plugin
+} // namespace WPEFramework
---
+++ OCIContainer/OCIContainerImplementation.cpp
@@ -0,0 +1,338 @@
+/**
+* If not stated otherwise in this file or this component's LICENSE
+* file the following copyright and licenses apply:
+*
+* Copyright 2024 RDK Management
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
+#include "OCIContainerImplementation.h"
+
+namespace WPEFramework
+{
+    namespace Plugin
+    {
+        SERVICE_REGISTRATION(OCIContainerImplementation, 1, 0);
+
+        OCIContainerImplementation::OCIContainerImplementation(): mDobbyInterface(nullptr)
+        {
+            LOGINFO("Create OCIContainerImplementation Instance");
+            mDobbyInterface = new DobbyInterface();
+            mDobbyInterface->initialize(this);
+        }
+
+        OCIContainerImplementation::~OCIContainerImplementation()
+        {
+            LOGINFO("Delete OCIContainerImplementation Instance");
+            mDobbyInterface->terminate();
+            delete mDobbyInterface;
+            mDobbyInterface = nullptr;
+        }
+
+        Core::hresult OCIContainerImplementation::Register(Exchange::IOCIContainer::INotification *notification)
+        {
+            ASSERT (nullptr != notification);
+        
+            mAdminLock.Lock();
+        
+            /* Make sure we can't register the same notification callback multiple times */
+            if (std::find(mOCIContainerNotification.begin(), mOCIContainerNotification.end(), notification) == mOCIContainerNotification.end())
+            {
+                LOGINFO("Register notification");
+                mOCIContainerNotification.push_back(notification);
+                notification->AddRef();
+            }
+        
+            mAdminLock.Unlock();
+        
+            return Core::ERROR_NONE;
+        }
+        
+        Core::hresult OCIContainerImplementation::Unregister(Exchange::IOCIContainer::INotification *notification )
+        {
+            Core::hresult status = Core::ERROR_GENERAL;
+        
+            ASSERT (nullptr != notification);
+        
+            mAdminLock.Lock();
+        
+            /* Make sure we can't unregister the same notification callback multiple times */
+            auto itr = std::find(mOCIContainerNotification.begin(), mOCIContainerNotification.end(), notification);
+            if (itr != mOCIContainerNotification.end())
+            {
+                (*itr)->Release();
+                LOGINFO("Unregister notification");
+                mOCIContainerNotification.erase(itr);
+                status = Core::ERROR_NONE;
+            }
+            else
+            {
+                LOGERR("notification not found");
+            }
+        
+            mAdminLock.Unlock();
+        
+            return status;
+        }
+        
+        void OCIContainerImplementation::dispatchEvent(EventNames event, const JsonObject &params)
+        {
+            Core::IWorkerPool::Instance().Submit(Job::Create(this, event, params));
+        }
+        
+        void OCIContainerImplementation::Dispatch(EventNames event, const JsonObject params)
+        {
+             //TODO: Convert params to proper values and pass
+             mAdminLock.Lock();
+        
+             std::list<Exchange::IOCIContainer::INotification*>::const_iterator index(mOCIContainerNotification.begin());
+        
+             switch(event)
+             {
+                 case OCICONTAINER_EVENT_CONTAINER_STARTED:
+                    while (index != mOCIContainerNotification.end())
+                    {
+                        string containerId("");
+                        string name("");
+                         (*index)->OnContainerStarted(containerId, name);
+                         ++index;
+                    }
+                    break;
+                 case OCICONTAINER_EVENT_CONTAINER_STOPPED:
+                    while (index != mOCIContainerNotification.end())
+                    {
+                        string containerId("");
+                        string name("");
+                         (*index)->OnContainerStopped(containerId, name);
+                         ++index;
+                    }
+                    break;
+                 case OCICONTAINER_EVENT_CONTAINER_FAILED:
+                    while (index != mOCIContainerNotification.end())
+                    {
+                        string containerId("");
+                        string name("");
+                         uint32_t error = 0;
+                         (*index)->OnContainerFailed(containerId, name, error);
+                         ++index;
+                    }
+                    break;
+                 case OCICONTAINER_EVENT_STATE_CHANGED:
+                    while (index != mOCIContainerNotification.end())
+                    {
+                        string containerId("");
+                        ContainerState state(TERMINATED);
+                        (*index)->OnContainerStateChanged(containerId, state);
+                        ++index;
+                    }
+                    break;
+        
+                 default:
+                    LOGWARN("Event[%u] not handled", event);
+                    break;
+             }
+        
+             mAdminLock.Unlock();
+        }
+        
+
+        Core::hresult OCIContainerImplementation::ListContainers(string& containers, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->listContainers(containers, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::GetContainerInfo(const string& containerId, string& info, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->getContainerInfo(containerId, info, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::GetContainerState(const string& containerId, ContainerState& state, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            std::string stateString;
+            success = mDobbyInterface->getContainerState(containerId, stateString, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            //TODO Convert from statestring to state
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::StartContainer(const string& containerId, const string& bundlePath, const string& command, const string& westerosSocket, uint32_t& descriptor, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->startContainer(containerId, bundlePath, command, westerosSocket, descriptor, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::StartContainerFromDobbySpec(const string& containerId, const string& dobbySpec, const string& command, const string& westerosSocket, uint32_t& descriptor, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->startContainerFromDobbySpec(containerId, dobbySpec, command, westerosSocket, descriptor, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::StopContainer(const string& containerId, bool force, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->stopContainer(containerId, force, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::PauseContainer(const string& containerId, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->pauseContainer(containerId, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::ResumeContainer(const string& containerId, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->resumeContainer(containerId, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::HibernateContainer(const string& containerId, const string& options, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->hibernateContainer(containerId, options, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::WakeupContainer(const string& containerId, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->wakeupContainer(containerId, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::ExecuteCommand(const string& containerId, const string& options, const string& command, bool& success ,string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->executeCommand(containerId, options, command, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::Annotate(const string& containerId, const string& key, const string& value, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->annotate(containerId, key, value, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::RemoveAnnotation(const string& containerId, const string& key, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->removeAnnotation(containerId, key, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::Mount(const string& containerId, const string& source, const string& target, const string& type, const string& options, bool& success ,string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->mount(containerId, source, target, type, options, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        Core::hresult OCIContainerImplementation::Unmount(const string& containerId, const string& target, bool& success, string& errorReason)
+        {
+            Core::hresult status = Core::ERROR_NONE;
+            success = mDobbyInterface->unmount(containerId, target, errorReason);
+            if (!success)
+            {
+                status = Core::ERROR_GENERAL;    
+            }
+            return status;
+        }
+
+        void OCIContainerImplementation::onContainerStarted(JsonObject& data)
+        {
+            dispatchEvent(OCIContainerImplementation::EventNames::OCICONTAINER_EVENT_CONTAINER_STARTED, data);
+        }
+
+        void OCIContainerImplementation::onContainerStopped(JsonObject& data)
+        {
+            dispatchEvent(OCIContainerImplementation::EventNames::OCICONTAINER_EVENT_CONTAINER_STOPPED, data);
+        }
+
+        void OCIContainerImplementation::onContainerFailed(JsonObject& data)
+        {
+            dispatchEvent(OCIContainerImplementation::EventNames::OCICONTAINER_EVENT_CONTAINER_FAILED, data);
+        }
+
+        void OCIContainerImplementation::onContainerStateChange(JsonObject& data)
+        {
+            dispatchEvent(OCIContainerImplementation::EventNames::OCICONTAINER_EVENT_STATE_CHANGED, data);
+        }
+
+    } /* namespace Plugin */
+} /* namespace WPEFramework */
