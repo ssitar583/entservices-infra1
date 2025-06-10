9217bd2805b9c4d1d738ee3dd46ea912bdd90206
Dosakayala Nagalakshmi <nadosakayala@synamedia.com>
2025-06-10 10:29:21 +0530

    Added additional L1 tests cases for ContentPin failure scenorios

--- Tests/L1Tests/tests/test_UserSettings.cpp
+++ Tests/L1Tests/tests/test_UserSettings.cpp
@@ -642,18 +642,34 @@ TEST_F(UserSettingsTest, GetVoiceGuidanceHints_Success)
     EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("getVoiceGuidanceHints"), _T("{}"), response));
 }
 
-TEST_F(UserSettingsTest, SetContentPin_Failure)
-{
-    EXPECT_CALL(*p_store2Mock, SetValue(::testing::_, ::testing::_, ::testing::_, ::testing::_, ::testing::_))
-        .WillOnce(::testing::Return(Core::ERROR_GENERAL));
-    EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setContentPin"), _T("{}"), response));
-}
-
 TEST_F(UserSettingsTest, SetContentPin_Success)
 {
     EXPECT_CALL(*p_store2Mock, SetValue(::testing::_, ::testing::_, ::testing::_, ::testing::_, ::testing::_))
         .WillOnce(::testing::Return(Core::ERROR_NONE));
-    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setContentPin"), _T("{}"), response));
+    EXPECT_EQ(Core::ERROR_NONE, handler.Invoke(connection, _T("setContentPin"), _T("{\"contentPin\":"1234"}"), response));
+}
+
+TEST_F(UserSettingsTest, SetContentPin_Failure)
+{
+    EXPECT_CALL(*p_store2Mock, SetValue(::testing::_, ::testing::_, ::testing::_, ::testing::_, ::testing::_))
+        .WillOnce(::testing::Return(Core::ERROR_GENERAL));
+
+    EXPECT_EQ(Core::ERROR_GENERAL, handler.Invoke(connection, _T("setContentPin"), _T("{\"contentPin\":"1234"}"), response));
+}
+
+TEST_F(UserSettingsTest, SetContentPin_FailureWithAlphanumericInput)
+{
+    EXPECT_EQ(Core::ERROR_INVALID_PARAMETER, handler.Invoke(connection, _T("setContentPin"), _T("{\"contentPin\":"12a4"}"), response));
+}
+
+TEST_F(UserSettingsTest, SetVoiceGuidanceRate_FailureThreeDigitNumber)
+{
+    EXPECT_EQ(Core::ERROR_INVALID_PARAMETER, handler.Invoke(connection, _T("setContentPin"), _T("{\"contentPin\":"123"}"), response));
+}
+
+TEST_F(UserSettingsTest, SetVoiceGuidanceRate_FailureFiveDigitNumber)
+{
+    EXPECT_EQ(Core::ERROR_INVALID_PARAMETER, handler.Invoke(connection, _T("setContentPin"), _T("{\"contentPin\":"12345"}"), response));
 }
 
 TEST_F(UserSettingsTest, GetContentPin_Failure)
