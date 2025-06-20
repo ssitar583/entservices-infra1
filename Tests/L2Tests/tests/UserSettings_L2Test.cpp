7a2f328e935e59f1f91a8fb7d321e9cd24fbfe2e
kprathyusha <kprathyusha@synamedia.com>
2025-06-20 14:25:04 +0530

    uncomment the test case

--- Tests/L2Tests/tests/UserSettings_L2Test.cpp
+++ Tests/L2Tests/tests/UserSettings_L2Test.cpp
@@ -735,7 +735,7 @@ MATCHER_P(MatchRequestStatusDouble, expected, "")
     return expected == actual;
 
 }
-#if 0
+
 TEST_F(UserSettingTest, SetAndGetMethodsUsingJsonRpcConnectionSuccessCase)
 {
     JSONRPC::LinkType<Core::JSON::IElement> jsonrpc(USERSETTING_CALLSIGN, USERSETTINGL2TEST_CALLSIGN);
@@ -2854,7 +2854,7 @@ TEST_F(UserSettingTest, PersistentstoreIsDeactivatedErrorCase)
         }
     }
 }
-#endif
+
 TEST_F(UserSettingTest, PersistentstoreIsNotActivatedWhileUserSettingsActivatingErrorCase)
 {
     uint32_t status = Core::ERROR_GENERAL;
