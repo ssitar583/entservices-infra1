3700679d13526436820c96d0de5cffdbeb0d44b0
pavithraviswa <pavithrav9208@gmail.com>
2025-06-24 11:23:26 +0530

    Update test_RunTimeManager.cpp

--- Tests/L1Tests/tests/test_RunTimeManager.cpp
+++ Tests/L1Tests/tests/test_RunTimeManager.cpp
@@ -29,6 +29,7 @@
 #include "OCIContainerMock.h"
 #include "WindowManagerMock.h"
 #include "WorkerPoolImplementation.h"
+#include <fstream>
 
 #define TEST_LOG(x, ...) fprintf(stderr, "\033[1;32m[%s:%d](%s)<PID:%d><TID:%d>" x "\n\033[0m", __FILE__, __LINE__, __FUNCTION__, getpid(), gettid(), ##__VA_ARGS__); fflush(stderr);
 
