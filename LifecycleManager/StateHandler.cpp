a23ec5bf4841f410617bc66d28b90db61475f403
IshvarKR <ishvarrr01@gmail.com>
2025-07-03 11:44:31 +0000

    RDKEMW-2806: [AI2.0][LCM]L1 tests for lifecycle manager run in CI framework
    Reason for change: Addition of L1 test cases for Lifecycle Manager
    Test Procedure: None
    Priority: P2
    Risks: None
    Signed-off-by: IshvarAdithya_KavasseryRamanathan@comcast.com

--- LifecycleManager/StateHandler.cpp
+++ LifecycleManager/StateHandler.cpp
@@ -72,7 +72,6 @@ namespace WPEFramework
             State* state = nullptr;
             switch (lifeCycleState)
             {
-            DEBUG_PRINTF("ERROR: RDKEMW-2806");
 	        case Exchange::ILifecycleManager::LifecycleState::UNLOADED:
                     state = new UnloadedState(context);
                     break;
@@ -303,7 +302,6 @@ namespace WPEFramework
         DEBUG_PRINTF("ERROR: RDKEMW-2806");
         return result;
     }
-    DEBUG_PRINTF("ERROR: RDKEMW-2806");
 
     } /* namespace Plugin */
 } /* namespace WPEFramework */
