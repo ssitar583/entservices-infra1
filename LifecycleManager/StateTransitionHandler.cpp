1aaf6d77678d20ecb9afe8255625207a40ec050c
IshvarKR <ishvarrr01@gmail.com>
2025-07-13 07:49:03 +0000

    RDKEMW-2806: [AI2.0][LCM]L1 tests for lifecycle manager run in CI framework
    Reason for change: Addition of L1 test cases for Lifecycle Manager
    Test Procedure: None
    Priority: P2
    Risks: None
    Signed-off-by: IshvarAdithya_KavasseryRamanathan@comcast.com

--- LifecycleManager/StateTransitionHandler.cpp
+++ LifecycleManager/StateTransitionHandler.cpp
@@ -118,7 +118,7 @@ namespace WPEFramework
             gRequestMutex.lock();
             sRunning = false;
             gRequestMutex.unlock();
-            sem_post(&gRequestSemaphore);
+            sem_destroy(&gRequestSemaphore);
             requestHandlerThread.join();
 	    DEBUG_PRINTF("StateTransitionHandler terminates");
 	}
